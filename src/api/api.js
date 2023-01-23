797a31143bc6d366c3158e6adaf47e56c637f0c0
ClaraSantosmf <clarasantosmf@gmail.com>
2023-01-23 18:02:15 -0300

    listar repositórios e adicionar um select

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,17 @@
 export const api = {
-    async search_users(searchstring) {
-    const result = await fetch(`https://api.github.com/search/users?q=${searchstring}`)
-    return await result.json()
+  async search_repo(username, page) {
+    if (!page) {
+        page = 1;
+    }
+    const result = await fetch(
+      `https://api.github.com/users/${username}/repos?page=${page}`
+    );
+    return await result.json();
+  },
+  async search_users(searchstring) {
+    const result = await fetch(
+      `https://api.github.com/search/users?q=${searchstring}`
+    );
+    return await result.json();
   },
 };
--- src/components/githubrepo.vue
+++ src/components/githubrepo.vue
@@ -1,22 +1,38 @@
 <template>
-    <v-autocomplete
-      v-model="user"
-      :items="userlist"
-      :loading="userloading"
-      :search-input.sync="usersearch"
-      item-text="login"
-      color="white"
-      hide-no-data
-      label="Buscador de Github Repo"
-      placeholder="Pesquise o nome do usuário github"
-      prepend-icon="mdi-database-search"
-      hide-selected
-    />
+  <div>
+    <div>
+      <v-autocomplete
+        v-model="user"
+        :items="userlist"
+        :loading="userloading"
+        :search-input.sync="usersearch"
+        item-text="login"
+        color="white"
+        hide-no-data
+        label="Buscador de Github Repo"
+        placeholder="Pesquise o nome do usuário github"
+        prepend-icon="mdi-github"
+        hide-selected
+      />
+    </div>
+    <div>
+      <v-select
+        v-model="repo"
+        :items="repolist"
+        item-text="name"
+        label="Selecione o repositório"
+        persistent-hint
+        return-object
+        single-line
+        prepend-icon="mdi-source-repository"
+      ></v-select>
+    </div>
+  </div>
 </template>
 
 <script>
 import { debouncerdecorator } from "@/helpers/debouncer.js";
-import { api } from "@/api/api.js"
+import { api } from "@/api/api.js";
 
 export default {
   data: () => ({
@@ -24,19 +40,29 @@ export default {
     userlist: [],
     userloading: false,
     usersearch: null,
+    repo: null,
+    repolist: [],
   }),
   methods: {
     procuraUsuarioGH: debouncerdecorator(async function () {
       this.userloading = true;
-      const data = await api.search_users(this.usersearch)
-          this.userlist = data.items
-          this.userloading = false
+      const data = await api.search_users(this.usersearch);
+      this.userlist = data.items;
+      this.userloading = false;
     }, 500),
-},
-    watch: {
-      usersearch() {
-        this.procuraUsuarioGH();
-      }
-    }
-  }
+    async listaRepositorios() {
+      const data = await api.search_repo(this.user)
+      this.repolist = data
+      debugger
+    },
+  },
+  watch: {
+    usersearch() {
+      this.procuraUsuarioGH();
+    },
+    user() {
+      this.listaRepositorios();
+    },
+  },
+};
 </script>
