7fb08e94eb19744e776ea737c4e9ea7305053126
ClaraSantosmf <clarasantosmf@gmail.com>
2023-01-25 20:56:24 -0300

    Adicionando o map

--- src/components/githubpages.vue
+++ src/components/githubpages.vue
@@ -1,7 +1,7 @@
 <template>
   <v-treeview
     v-model="tree"
-    :load-children="abrido"
+    :load-children="criandoFilhos"
     :items="items"
     activatable
     item-key="name"
@@ -9,72 +9,72 @@
     :open.sync="open"
   >
     <template v-slot:prepend="{ item, open }">
-        <v-icon v-if="item.type == 'dir'">
-          {{ open ? "mdi-folder-open" : "mdi-folder" }}
-        </v-icon>
-        <v-icon v-else>
-          {{ files[item.ext] }}
-        </v-icon>
+      <v-icon v-if="item.type == 'dir'">
+        {{ open ? "mdi-folder-open" : "mdi-folder" }}
+      </v-icon>
+      <v-icon v-else>
+        {{ files[item.ext] }}
+      </v-icon>
     </template>
   </v-treeview>
 </template>
 
 <script>
-import {api} from "~api"
-  export default {
-    props: ["repo"],
-    data: () => ({
-      open: [],
-      files: {
-        html: 'mdi-language-html5',
-        js: 'mdi-nodejs',
-        json: 'mdi-code-json',
-        md: 'mdi-language-markdown',
-        pdf: 'mdi-file-pdf',
-        png: 'mdi-file-image',
-        txt: 'mdi-file-document-outline',
-        xls: 'mdi-file-excel',
-        aleatorio: 'mdi-airballoon-outline'
-      },
-      tree: [],
-      items: []
-    }),
-    methods: {
-        async listandoHome() {
-            const files = await api.search_file(this.repo.full_name)
-            this.items = this.organizador(files)
-        },
-        organizador(files){
-          for(let i=0; i < files.length; i++) {
-            let ext = this.extensao(files[i].name)
-            files[i]['ext']= ext
-            if (files[i].type =='dir'){
-              files[i].children = []
-            }
-          }
-          return files
-        },
-        extensao(name){
-           let iconextensao = name.includes('.')? name.split('.').pop(): ""
-           if (this.files[iconextensao]){
-             return iconextensao
-           }
-           iconextensao = 'aleatorio'
-           return iconextensao
-        },
-        abrido(item){
-          if (item.type == 'dir'){
-            api.search_file(this.repo.full_name, item.path).then(resposta =>{
-              item.children = this.organizador(resposta)
-            } )
-          }
-          return
-        }
-      },
-    watch:{
-        repo(){
-            this.listandoHome()
-        }
+import { api } from "~api";
+export default {
+  props: ["repo"],
+  data: () => ({
+    open: [],
+    files: {
+      html: "mdi-language-html5",
+      js: "mdi-nodejs",
+      json: "mdi-code-json",
+      md: "mdi-language-markdown",
+      pdf: "mdi-file-pdf",
+      png: "mdi-file-image",
+      txt: "mdi-file-document-outline",
+      xls: "mdi-file-excel",
+      aleatorio: "mdi-airballoon-outline",
     },
-  }
+    tree: [],
+    items: [],
+  }),
+  methods: {
+    async listandoHome() {
+      const files = await api.search_file(this.repo.full_name);
+      this.items = this.organizador(files);
+      debugger
+    },
+    organizador(files) { 
+      return files.map((file) => {
+        file.ext = this.extensao(file.name);
+        if (file.type == "dir") {
+          file.children = [];
+        }
+        return file
+      });
+    },
+    extensao(name) {
+      let iconextensao = name.includes(".") ? name.split(".").pop() : "";
+      if (this.files[iconextensao]) {
+        return iconextensao;
+      }
+      iconextensao = "aleatorio";
+      return iconextensao;
+    },
+    criandoFilhos(item) {
+      if (item.type == "dir") {
+        api.search_file(this.repo.full_name, item.path).then((resposta) => {
+          item.children = this.organizador(resposta);
+        });
+      }
+      return;
+    },
+  },
+  watch: {
+    repo() {
+      this.listandoHome();
+    },
+  },
+};
 </script>
